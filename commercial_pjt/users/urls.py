8fa90403f8e4ba840fd285263ffe4c4598176117
godo129 <76652929+godo129@users.noreply.github.com>
2022-05-17 09:59:45 +0900

    Add files via upload

---
+++ commercial_pjt/users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+app_name='users'
+
+urlpatterns = [
+    path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name="login"),
+    path('login/kakao/', views.kakao_login, name="kakao-login"),
+    path('login/kakao/callback/', views.kakao_login_callback, name="kakao-callback"),
+    path('logout/', views.logout, name="logout"),
+    path('logout/kakao/', views.kakao_logout, name="kakao-logout"),
+]
